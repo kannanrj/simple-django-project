59664def7e2d4aa9952d3b0f379606ead8435519
Nikhil raj <106697827+kannanrj@users.noreply.github.com>
2025-08-03 21:01:04 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,22 +1,23 @@
-#!/usr/bin/env python
-import os
-import sys
+from flask import Flask, request
+from datetime import datetime
+import json
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "panorbit.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
-    execute_from_command_line(sys.argv)
+app = Flask(__name__)
+
+@app.route('/')
+def get_time_ip():
+    x_forwarded_for = request.headers.get('X-Forwarded-For')
+    if x_forwarded_for:
+        client_ip = x_forwarded_for.split(',')[0].strip()
+    else:
+        client_ip = request.remote_addr
+
+    response = {
+        "timestamp": datetime.utcnow().isoformat() + "Z",
+        "ip": client_ip
+    }
+
+    return json.dumps(response), 200, {'Content-Type': 'application/json'}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
